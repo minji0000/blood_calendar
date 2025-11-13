8d4fb77aef2ff301902c364a8391c8b1327d6e6d
minji0000 <bluurmi@gmail.com>
2025-11-14 00:01:52 +0900

    feat: Initial project setup with Spring Boot

---
+++ src/main/java/org/calendar/blood_calendar/BloodCalendarApplication.java
@@ -0,0 +1,13 @@
+package org.calendar.blood_calendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodCalendarApplication.class, args);
+	}
+
+}
